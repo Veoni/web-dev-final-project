31f7f93d1ef6a6e7c08358553f870d150a86bb70
Veoni <sveoni12@gmail.com>
2013-07-13 11:37:24 -0400

    Altered code in content-footer.php so taxonomies would be visible on custom posts

---
+++ wordpress/wp-content/themes/magazine-basic-child/content-footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying article footers
+ *
+ * @since 3.0.0
+ */
+global $mb_content_area;
+if ( is_singular() && 'sidebar' != $mb_content_area ) : ?>
+    <footer class="entry">
+	    <?php
+	   	wp_link_pages( array( 'before' => '<p id="pages">' . __( 'Pages:', 'magazine-basic' ) ) );
+
+	   		the_terms( $post->ID, 'year', '<p class="tags">Year: ', ' ', '</p>' );
+			the_terms( $post->ID, 'region', '<p class="tags">Region: ', ' ', '</p>' );
+
+		edit_post_link( __( '(edit)', 'magazine-basic' ), '<p>', '</p>' );
+		?>
+	</footer><!-- .entry -->
+	<?php
+endif;
+
+
