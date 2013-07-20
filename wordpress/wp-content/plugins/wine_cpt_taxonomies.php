98b4e97954080b9c2d61ece644de6b5093332762
Veoni <sveoni12@gmail.com>
2013-07-20 11:10:01 -0400

    added new header logo

--- wordpress/wp-content/plugins/wine_cpt_taxonomies.php
+++ wordpress/wp-content/plugins/wine_cpt_taxonomies.php
@@ -42,40 +42,6 @@ function sv_register_cpt_wine() {
 
 }
 
-
-/*
-Create custom taxonomy for region
-@Wine
-*/
-
-add_action( 'init', 'sv_region_register_wine_taxonomies', 0 );
-
-function sv_region_register_wine_taxonomies() {
-    // Add new taxonomy, keep it non-hierarchical (like tags)
-    $labels = array(
-        'name'                          => __( 'Region', 'region' ),
-        'singular_name'                 => __( 'Regions', 'region' ),
-        'search_items'                  => __( 'Search Regions', 'region' ),
-        'all_items'                     => __( 'All Regions', 'region' ),
-        'edit_item'                     => __( 'Edit Region', 'region' ), 
-        'update_item'                   => __( 'Update Region', 'region' ),
-        'add_new_item'                  => __( 'Add New Region', 'region' ),
-        'new_item_name'                 => __( 'New Region Name', 'region' ),
-        'separate_items_with_commas'    => __( 'Separate regions with commas', 'region' ),
-        'menu_name'                     => __( 'Regions', 'region' ),
-    );  
-
-
-    register_taxonomy( 'region', array( 'wine' ), array(
-        'hierarchical'      => false,
-        'labels'            => $labels,
-        'show_ui'           => true,
-        'show_admin_column' => true,
-        'query_var'         => true,
-        'rewrite'           => array( 'slug' => 'region' ),
-    ));
-}
-
 /*
 Create custom taxonomy for year
 @Wine
@@ -107,6 +73,39 @@ function sv_year_register_wine_taxonomies() {
         'rewrite'           => array( 'slug' => 'year' ),
     ));
 
+}
+
+/*
+Create custom taxonomy for region
+@Wine
+*/
+
+add_action( 'init', 'sv_region_register_wine_taxonomies', 0 );
+
+function sv_region_register_wine_taxonomies() {
+    // Add new taxonomy, keep it non-hierarchical (like tags)
+    $labels = array(
+        'name'                          => __( 'Region', 'region' ),
+        'singular_name'                 => __( 'Regions', 'region' ),
+        'search_items'                  => __( 'Search Regions', 'region' ),
+        'all_items'                     => __( 'All Regions', 'region' ),
+        'edit_item'                     => __( 'Edit Region', 'region' ), 
+        'update_item'                   => __( 'Update Region', 'region' ),
+        'add_new_item'                  => __( 'Add New Region', 'region' ),
+        'new_item_name'                 => __( 'New Region Name', 'region' ),
+        'separate_items_with_commas'    => __( 'Separate regions with commas', 'region' ),
+        'menu_name'                     => __( 'Region', 'region' ),
+    );  
+
+
+    register_taxonomy( 'region', array( 'wine' ), array(
+        'hierarchical'      => false,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'region' ),
+    ));
 }
 
 add_filter( 'pre_get_posts', 'my_get_posts' );
